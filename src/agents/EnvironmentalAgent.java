2c0ebef718976b8cf1c4fb5f10f1b635df683e03
BenoitV <benoit.villain@etu.utc.fr>
2016-03-27 14:45:40 +0200

    modif

--- src/agents/EnvironmentalAgent.java
+++ src/agents/EnvironmentalAgent.java
@@ -1,14 +1,16 @@
 package agents;
 
-import java.io.StringWriter;
+
 import java.util.ArrayList;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import Class_For_JSON.OperationResult;
 import SudokuSim.Cell;
 import SudokuSim.SudokuManager;
-import jade.core.AID;
+
 import jade.core.behaviours.CyclicBehaviour;
+import jade.core.behaviours.OneShotBehaviour;
 import jade.domain.DFService;
 import jade.domain.FIPAException;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
@@ -26,14 +28,14 @@ public class EnvironmentalAgent extends Agent{
 		//On récupère le sudoku du fichier
 		System.out.println("Agent Simulation. ");
 		SudokuInt = (ArrayList<Integer>) this.getArguments()[0];
-		
+
 		//On le convertit comme une grille de cells (val +listes des possibles)
-	    for(int i = 0; i < SudokuInt.size(); i++)
-	    {
-	    	Cell element = new Cell(SudokuInt.get(i));
-	    	Sudoku.add(i, element);
-	    }
-	    
+		for(int i = 0; i < SudokuInt.size(); i++)
+		{
+			Cell element = new Cell(SudokuInt.get(i));
+			Sudoku.add(i, element);
+		}
+
 		DFAgentDescription dafd = new DFAgentDescription();
 		dafd.setName(getAID());
 		ServiceDescription sd = new ServiceDescription();
@@ -50,45 +52,60 @@ public class EnvironmentalAgent extends Agent{
 
 		System.out.println("Agent Environmental. ");
 		System.out.println("My name is "+ this.getLocalName()); 	          
-		//addBehaviour(new EBehaviour());
+		addBehaviour(new WaitBehaviour());
 
 	}
 
+	class WaitBehaviour extends CyclicBehaviour{
 
-}
-
-class EBehaviour extends CyclicBehaviour{
-
-	jade.core.Agent thisagent;
-
-	public EBehaviour() {
-		// TODO Auto-generated constructor stub
-
+		@Override
+		public void action() {
+			// On attend la réception de message de type REQUEST
+			MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.REQUEST);
+			ACLMessage message = myAgent.receive(mt);
+			if (message != null){
+				// A la reception, on lance un OneShotBehaviour qui s'occupe de traiter le message et de répondre
+				myAgent.addBehaviour(new MajBehaviour(message));
+			}else{
+				block();
+			}
+		}
 	}
 
-	@Override
-	public void action() {
-		
-		
-		
-		MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.REQUEST);
-		// TODO Auto-generated method stub
-		ACLMessage message = thisagent.receive(mt);
-		System.out.println("Message recu. ");
+	class MajBehaviour extends OneShotBehaviour{
+		private String mess;
+		private ACLMessage message;
+		private ArrayList<Cell> SudokuSimul;
 
-		
-		
-		
-		if (message != null){
+		// Constructor
+		public MajBehaviour(ACLMessage message2) {
+			this.message = message2;
+			this.mess = message.getContent();
+			this.SudokuSimul =  new ArrayList<Cell>();
+		}
 
+		// Task to do
+		public void action() {
+			// On deserialise le message
+			ObjectMapper mapper = new ObjectMapper();
+			try {
+				OperationResult ort = mapper.readValue(mess,OperationResult.class);
+				SudokuSimul = ort.getValue();
+			}
+			catch(Exception ex) {
+				System.out.println("EXCEPTION" + ex.getMessage());
+			}
 			
+			// On met a jour le Sudoku.
+			Sudoku = SudokuSimul;
 			
-			
-			
-		}else
-			block();
+			// Si le Sudoku est terminé
+			if (manager.SudokuIsFinished(Sudoku)){
+				// On affiche le résultat
+				manager.AfficheSudoku(Sudoku);
+
+			}
+		}
+
 	}
-
-
-
 }
